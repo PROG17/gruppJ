ac7f20d4bb45faf9ea0244403aae9d94d6756b41
Gustav Cleveman <31858743+GustavCleveman@users.noreply.github.com>
2017-09-14 13:22:51 +0200

    tog bort test-sök

--- sudoko/Game.cs
+++ sudoko/Game.cs
@@ -52,24 +52,7 @@ namespace sudoko
                 }
             }
         }
-        public void Solve()
-        {
-            bool solved = false;
-            while (solved == false)
-            {
-                for (int i = 0; i < 9; i++)
-                {
-                    for (int j = 0; j < 9; j++)
-                    {
-                        if (board[i, j] == 0)
-                        {
-
-                        }
-                    }
-
-                }
-            }
-        }
+       
     }
 }
         
